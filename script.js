ac9ace4ac02b625ac2c916c5134563c3dca57663
nicolae-co <cojocarugithub@gmail.com>
2023-07-18 00:27:54 +0300

    accounts changes

--- script.js
+++ script.js
@@ -23,6 +23,11 @@ const account1 = {
   username: "a",
   password: "a",
   logo: "./assets/logoEndzone.svg",
+  games: {
+    account2: "",
+    account3: "",
+    account4: "",
+  },
 };
 
 const account2 = {
@@ -30,6 +35,11 @@ const account2 = {
   username: "b",
   password: "b",
   logo: "./assets/logoTuf.png",
+  games: {
+    account1: "",
+    account3: "",
+    account4: "",
+  },
 };
 
 const account3 = {
@@ -37,6 +47,11 @@ const account3 = {
   username: "c",
   password: "c",
   logo: "./assets/logoJets.png",
+  games: {
+    account1: "",
+    account2: "",
+    account4: "",
+  },
 };
 
 const account4 = {
@@ -44,6 +59,11 @@ const account4 = {
   username: "d",
   password: "d",
   logo: "./assets/logoNuf.png",
+  games: {
+    account1: "",
+    account2: "",
+    account3: "",
+  },
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -63,28 +83,7 @@ const inputLoginUsername = document.querySelector(".login__input--user");
 const inputLoginPass = document.querySelector(".login__input--pin");
 
 let currentAccount;
-
-btnLogin.addEventListener("click", function (event) {
-  event.preventDefault();
-  currentAccount = accounts.find(
-    (acc) => acc.username === inputLoginUsername.value
-  );
-  if (currentAccount?.password === inputLoginPass.value) {
-    containerLogin.classList.remove("visible");
-    containerLogin.classList.add("hidden");
-    containerDashboard.classList.remove("hidden");
-    containerDashboard.classList.add("visible");
-
-    dashboardTeamLogo.src = currentAccount.logo;
-
-    displayGames(currentAccount);
-
-    console.log(currentAccount);
-  }
-});
-
 const games = [];
-
 for (let i = 0; i < accounts.length; i++) {
   const team1 = accounts[i].team;
 
@@ -93,24 +92,15 @@ for (let i = 0; i < accounts.length; i++) {
     const team2 = accounts[j].team;
 
     // Create a new object with the combination and add it to the new array
-    const combination = [team1, team2];
+    const combination = {
+      [team1]: {
+        [team2]: "",
+      },
+      [team2]: { [team1]: "" },
+    };
     games.push(combination);
   }
 }
-
-const displayGames = function (team) {
-  for (let game of games) {
-    if (game.includes(team.team)) {
-      const match = document.createElement("h1");
-      match.dataset.team1 = game[0];
-      match.dataset.team2 = game[1];
-      match.className = "match";
-      match.textContent = `${game[0]} - ${game[1]}`;
-      dashboardMatches.appendChild(match);
-      console.log(dashboardMatches);
-    }
-  }
-};
 
 dashboardMatches.addEventListener("click", function (e) {
   const clicked = e.target.closest(".match");
@@ -127,6 +117,8 @@ dashboardMatches.addEventListener("click", function (e) {
 
   if (clicked) {
     openModal();
+    console.log(currentAccount);
+    updateFormModal();
   }
   // for (let i = 0; i < btnsOpenModal.length; i++) {
   //   btnsOpenModal[i].addEventListener("click", openModal);
@@ -135,3 +127,65 @@ dashboardMatches.addEventListener("click", function (e) {
   btnCloseModal.addEventListener("click", closeModal);
   overlay.addEventListener("click", closeModal);
 });
+
+btnLogin.addEventListener("click", function (event) {
+  event.preventDefault();
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
+  if (currentAccount?.password === inputLoginPass.value) {
+    containerLogin.classList.remove("visible");
+    containerLogin.classList.add("hidden");
+    containerDashboard.classList.remove("hidden");
+    containerDashboard.classList.add("visible");
+
+    dashboardTeamLogo.src = currentAccount.logo;
+  }
+
+  displayGames(currentAccount, currentAccount.team);
+  console.log(games);
+});
+
+const displayGames = function (key) {
+  for (let game of games) {
+    if (game.hasOwnProperty(key.team)) {
+      console.log("test");
+      const match = document.createElement("h1");
+      match.dataset.team1 = Object.keys(game)[0];
+      match.dataset.team2 = Object.keys(game)[1];
+      match.className = "match";
+      match.textContent = `${Object.keys(game)[0]} - ${Object.keys(game)[1]}`;
+      dashboardMatches.appendChild(match);
+    }
+  }
+};
+
+const updateFormModal = function () {
+  const markup = `
+
+  <form id="form">
+                    <label for="rules">Knowledge of the Rules</label>
+                    <input type="range" id="rules" min="0" max="4" />
+                    <label for="contact">Fouls and Body Contact</label>
+                    <input type="range" id="contact" min="0" max="4" />
+                    <label for="fair-minddness">Fair-Mindeness</label>
+                    <input type="range" id="fair-minddness" min="0" max="4" />
+                    <label for="self-control">Positive Attitude and Self Control</label>
+                    <input type="range" id="self-control" min="0" max="4" />
+                    <label for="communication">Communications</label>
+                    <input type="range" id="communication" min="0" max="4" />
+                  </form>`;
+  modal.insertAdjacentHTML("afterbegin", markup);
+};
+
+function filterByKeys(data, key1, key2) {
+  const matchingPairs = [];
+
+  for (const [index, obj] of Object.entries(data)) {
+    if (obj.hasOwnProperty(key1) && obj.hasOwnProperty(key2)) {
+      matchingPairs.push({ [index]: obj });
+    }
+  }
+
+  console.log(matchingPairs);
+}
